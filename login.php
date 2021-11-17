57d347acea9fcdd7540bb8b46383c5adda406636
FeliciaLeaton <fglfz8@mail.missouri.edu>
2021-11-17 10:29:46 -0600

    login.php working

--- login.php
+++ login.php
@@ -1,33 +1,87 @@
-<?php
- if(isset($_POST['submit'])
-  {
-   $email= ($_POST['email']);
-   $password = ($_POST['password']);
-    if(empty($username))
-    {
-     echo "Empty or invalid email address";
-    }
-     if(empty($password)){
-     echo "Enter your password"; 
-      }
-      $con = new MongoClient();
-     // Select Database
-     if($con)
-      {
-      $db = $con->nxtcert;
-      // Select Collection
-    $collection = $db->users;
-     $qry = array("username" => $username,"password" => md5($password));
-      $result = $collection->findOne($qry);
-    if($result){
-     echo "You are successully loggedIn";
-       }
-    else
-     { echo "unsuccessful";
-     }
+<?php 
 
-      } else { 
-      die("Mongo DB not connected");
-      } 
+session_start(); /* Starts the session */
+require '/var/vendor/autoload.php';
+// $client = new MongoDB\Client("mongodb://localhost:27017");
+// $collection = $client->nxtcert->users;
+
+echo "database selected";
+//$m= new MongoDB\Client("mongodb://127.0.0.1/");
+   // select a database
+
+/* Check Login form submitted */
+if(isset($_POST['submit'])){
+/* Define username and associated password array */
+    $logins = array('test@test' => '1234','user@user' => 'password');
+
+    /* Check and assign submitted Username and Password to new variable */
+    $Username = isset($_POST['email']) ? $_POST['email'] : '';
+    $Password = isset($_POST['password']) ? $_POST['password'] : '';
+
+
+    if (isset($logins[$Username]) && $logins[$Username] == $Password){
+        echo "logging in";
+        header("location:profile.php");
+
+        $_SESSION['UserData']['Username']=$Username;
+        
+        exit;
+    } else {
+        echo "invalid login";
+        header('Refresh: 2; URL = login.html');
+    }
+}
+
+$logins = array('test@test' => '1234','user@user' => 'password');
+
+/* Check Login form submitted */
+if(isset($_POST['submit'])){
+    /* Define username and associated password array */
+   
+    
+    /* Check and assign submitted Username and Password to new variable */
+    $Username = isset($_POST['email']) ? $_POST['regemail'] : '';
+    $Password = isset($_POST['password']) ? $_POST['regpassword'] : '';
+    
+    if (isset($logins[$Username]) && $logins[$Username] == $Password){
+        $_SESSION['UserData']['Username']=$Username;
+        header("location:profile.php");
+        exit;
+    } else {
+        echo "invalid login";
+        header('Refresh: 2; URL = login.html');
+    }
+    }
+
+
+    if(isset($_POST['register'])){
+
+
+        // $document = array( 
+        //    "title" => "MongoDB", 
+        //    "description" => "database", 
+        //    "likes" => 100,
+        //    "url" => "http://www.tutorialspoint.com/mongodb/",
+        //    "by" => "tutorials point"
+        // );
+         
+        // $collection->insert($document);
+        // echo "Document inserted successfully";
+        /* Define username and associated password array */
+        
+        
+        /* Check and assign submitted Username and Password to new variable */
+        $Username = isset($_POST['email']) ? $_POST['email'] : '';
+        $Password = isset($_POST['password']) ? $_POST['password'] : '';
+        
+        
+        if (array_push($logins, $Username, $Password)){
+            $_SESSION['UserData']['Username']=$Username;
+            header("location:profile.php");
+            exit;
+        } else {
+            echo "error";
+            header('Refresh: 2; URL = login.html');
         }
-      ?>
+        }
+?>
